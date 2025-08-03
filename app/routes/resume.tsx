3d36a8530fb4667956c71798853b8e2d53d751d1
Ashis Swain <ashisswainofficial77@gmail.com>
2025-08-04 00:43:19 +0530

    Resume Feedback Page

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -1,6 +1,9 @@
 import {Link, useNavigate, useParams} from 'react-router';
 import {useState, useEffect} from 'react';
 import {usePuterStore} from '~/lib/puter';
+import Summary from '~/components/Summary';
+import Details from '~/components/Details';
+import ATS from '~/components/ATS';
 
 export const meta = () => [
   { title: 'Resumind | Review' },
@@ -13,7 +16,7 @@ const resume = () => {
   const {id} = useParams();
   const [imageUrl, setImageUrl] = useState('');
   const [resumeUrl, setResumeUrl] = useState('');
-  const [feedback, setFeedback] = useState('');
+  const [feedback, setFeedback] = useState<Feedback | null>(null);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -76,7 +79,9 @@ const resume = () => {
               <h2 className="text-4xl text-black font-bold">Resume Review</h2>
               {feedback ? (
                 <div className="flex flex-col gap-8 animate-in fade-in duration-1000">
-                  Summary ATS Details
+                  <Summary feedback={feedback} />
+                  <ATS score={feedback.ATS.score || 0} suggestions={feedback.ATS.tips || []} />
+                  <Details feedback={feedback} />
                 </div>
               ): (
                 <img src="/images/resume-scan-2.gif" alt="scan" className="w-full" />
